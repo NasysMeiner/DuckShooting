749fe5f3997588830ccb38c0138e2751b3d6aaa7
NasysMeiner <iguana10053@gmail.com>
2023-10-02 00:00:36 +0300

    NewCameraMovement
    Blocking the camera in extreme points

--- Assets/Scripts/CameraWindow/CameraWindow.cs
+++ Assets/Scripts/CameraWindow/CameraWindow.cs
@@ -4,10 +4,17 @@ public class CameraWindow : MonoBehaviour
 {
     [SerializeField] private float _startSpeed = 0.0001f;
     [SerializeField] private float _minusSpeed = 0.0001f;
+    [SerializeField] private Transform _leftPoint;
+    [SerializeField] private Transform _rightPoint;
+    [SerializeField] private Transform _upPoint;
+    [SerializeField] private Transform _downPoint;
 
     private Movement _player;
     private bool _isInside = true;
     private bool _isMovementCamera = false;
+    private Vector3 _direction => (new Vector3(_player.transform.position.x - transform.position.x, _player.transform.position.y - transform.position.y, _player.transform.position.z - transform.position.z)).normalized;
+
+    private bool _isInBounds => _leftPoint.position.x < transform.position.x + _direction.x * _startSpeed && _rightPoint.position.x > transform.position.x + _direction.x * _startSpeed && _upPoint.position.y > transform.position.y + _direction.y * _startSpeed && _downPoint.position.y < transform.position.y + _direction.y * _startSpeed ? true : false;
 
     private void OnTriggerExit(Collider other)
     {
@@ -18,7 +25,7 @@ public class CameraWindow : MonoBehaviour
             _isMovementCamera = true;
         }
     }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent(out BorderPlayer player))
@@ -27,23 +34,36 @@ public class CameraWindow : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        if(_player != null && transform != null)
+            Move(transform.position, _player.transform.position);
     }
 
-    private void Move()
+    private void Move(Vector3 camera, Vector3 target)
     {
         if (_isMovementCamera)
         {
             float speed = _startSpeed;
-
-            if (transform != null)
-                transform.position = Vector3.Lerp(transform.position, _player.transform.position, speed);
 
             if (_isInside)
                 speed -= _minusSpeed;
 
             if (speed <= 0)
                 _isMovementCamera = false;
+
+            CheckPath(speed, out Vector3 direction);
+
+            transform.position = new Vector3(camera.x + (target.x - camera.x) * direction.x, camera.y + (target.y - camera.y) * direction.y, camera.z + (target.z - camera.z) * direction.z);
         }
+    }
+
+    private void CheckPath(float speed, out Vector3 direction)
+    {
+        direction = new Vector3(speed, speed, speed);
+ 
+        if (transform.position.x + 2 * _direction.x * Time.fixedDeltaTime > _rightPoint.position.x || transform.position.x + 2 * _direction.x * Time.fixedDeltaTime < _leftPoint.position.x)
+            direction.x = 0;
+        
+        if(transform.position.y + 2 * _direction.y * Time.fixedDeltaTime > _upPoint.position.y || transform.position.y + 2 * _direction.y * Time.fixedDeltaTime < _downPoint.position.y)
+            direction.y = 0;
     }
 }
